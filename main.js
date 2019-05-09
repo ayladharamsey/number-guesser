3f9f365a43604ec0356b6107d0503401a19068cc
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-05-09 13:51:14 -0600

    update to initial function and moved js link in to the bottom of the body

--- main.js
+++ main.js
@@ -2,14 +2,16 @@ var minRange = document.querySelector('.min-range');
 var maxRange = document.querySelector('.max-range');
 var minValue = document.querySelector('#min-value');
 var maxValue = document.querySelector('#max-value');
-var updateBtn = document.querySelector('#update');
+var updateBtn = document.getElementById('update');
+console.log(updateBtn)
 
+updateBtn.addEventListener("click", updateRange);
 
-updateBtn.addEventListener("click", updateRange(){
-	minValue.innerText = minRange();
-});
+// updateBtn.addEventListener("click", updateRange(){
+// 	maxValue.innerText = maxRange();
+// });
 
-updateBtn.addEventListener("click", updateRange(){
-	maxValue.innerText = maxRange();
-});
-
+function updateRange(){
+	// minValue.innerText = minRange;
+	console.log('hello')
+}
